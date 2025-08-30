eccb29e91021a068bcaa246b1de4674d890776af
Nalla karunakar rao <97623030+KaranRocky@users.noreply.github.com>
2025-08-30 20:33:18 +0530

    Update StableFund.sol

--- contracts/StableFund.sol
+++ contracts/StableFund.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract ReserveVault {
     using SafeERC20 for IERC20;
 
-
     /// @notice Address of the vault manager
     address public immutable manager;
 
